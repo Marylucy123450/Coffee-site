0482d9842b55755fc56f74dc47f2d720290af8a5
Marylucy <marylucyatienoomenda.gmail.com>
2022-12-17 10:12:46 +0300

    editing js

--- index.js
+++ index.js
@@ -21,9 +21,3 @@ document.querySelector('#cart-btn').onclick = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
 }
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
